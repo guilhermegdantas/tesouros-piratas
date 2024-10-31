a9146c4e9a95ab78c5c277158de9d646d90ff629
Guilherme Gonçalves Dantas <guilhermedantas162@gmail.com>
2024-10-31 14:02:52 -0300

    commit inicial
    Primeiro commit

---
+++ dados.js
@@ -0,0 +1,74 @@
+let dados = [
+
+    {
+        titulo: "Baú de ouro de Thomas Cavendish",
+        descricao: "Em uma segunda expedição, em 1591, Cavendish partiu novamente em busca de riquezas, mas a missão foi um fracasso total. A frota enfrentou doenças, descontentamento entre os tripulantes e falhas nas embarcações. As dificuldades foram tão severas que ele foi forçado a retornar, sem alcançar os tesouros que procurava e sem reabastecer suas riquezas. <br> <strong>Local:</strong>Uma das teorias mais comuns é que Cavendish possa ter escondido parte de seu tesouro nas Filipinas, na costa da Califórnia ou em uma das ilhas do Pacífico. Algumas histórias indicam que ele teria escondido ouro e prata em baús enterrados em praias isoladas, longe do alcance dos espanhóis ou de rivais.",
+        link: "https://pt.wikipedia.org/wiki/Thomas_Cavendish",
+        local:"",
+        tags: "baú ouro thomas cavendish pirata"
+    },
+    {
+        titulo: "Ouro de Lima",
+        descricao: "Este não é exatamente um tesouro de pirata, mas ele foi capturado e redirecionado por piratas. Em 1820, um tesouro inca em ouro e joias foi levado de Lima para ser salvo dos conquistadores. No entanto, um capitão pirata chamado William Thompson teria assassinado a tripulação e escondido o tesouro. <br> <strong>Local:</strong> O Tesouro de Lima, como é conhecido, poderia estar na Ilha Cocos, na Costa Rica, um local frequentemente apontado como refúgio de tesouros perdidos.",
+        link: "https://forbes.com.br/forbeslife/2019/09/conheca-a-ilha-da-costa-rica-que-esconde-um-tesouro-de-us-1-bilhao/#:~:text=Tesouro%20de%20Lima,que%20colonizou%20parte%20das%20Am%C3%A9ricas.",
+        local:"",
+        tags: "ouro lima"
+    }
+    ,
+    {
+        titulo: "Fortuna do Barba Negra",
+        descricao: "Edward Teach, conhecido como Barba Negra, foi um pirata temido no início do século XVIII. Acredita-se que ele tenha acumulado uma fortuna em ouro, prata e joias saqueadas de navios ao longo da costa leste da América do Norte e do Caribe. <br> <strong>Local:</strong> Existem muitas especulações sobre onde ele poderia ter escondido seu tesouro, mas até hoje ninguém o encontrou. Um dos possíveis locais é a área da costa da Carolina do Norte.",
+        link: "https://pt.wikipedia.org/wiki/Barba-Negra",
+        local:"",
+        tags: "fortuna barba negra pirata"
+    },
+    {
+        titulo:"Tesouro de Francis Drake",
+        descricao : "Uma das lendas mais intrigantes em torno de Drake é a de que ele teria escondido parte de suas riquezas, especialmente em suas incursões na costa da América do Sul. Esse 'tesouro de Francis Drake' nunca foi encontrado e permanece uma incógnita. <br> <strong>Local:</strong> Diversas teorias indicam que Drake poderia ter escondido riquezas em locais estratégicos na costa do Panamá, da Colômbia ou até na Califórnia. Existem especulações sobre baús enterrados em cavernas ou em ilhas isoladas, com locais que teriam sido escolhidos de modo que só ele e sua tripulação conhecessem.",
+        link: "https://pt.wikipedia.org/wiki/Francis_Drake",
+        local:"",
+        tags: "tesouro francis drake sir pirata"
+    },
+    {
+        titulo:"Tesouro de Devonshire",
+        descricao : "Clavell é conhecido tanto por seu histórico de pirataria quanto por ter sido um poeta e autor. Após ser perdoado, ele teria escondido parte de sua fortuna para evitar atrair atenção, especialmente de autoridades. <br> <strong>Local:</strong> Devonshire (ou Devon), uma região montanhosa e costeira no sudoeste da Inglaterra, seria onde Clavell teria escondido o tesouro. Há quem diga que ele escolheu uma caverna ou um penhasco próximo ao mar, onde apenas alguém com conhecimento da área poderia localizar.",
+        link: "https://pt.wikipedia.org/wiki/Duque_de_Devonshire",
+        local: "",
+        tags: "devonshire tesouro"
+    },
+    {
+        titulo:"O Tesouro da Ilha da Trindade",
+        descricao : "A principal lenda que cerca a Ilha da Trindade é a de que o pirata inglês William Dampier teria escondido um tesouro nela. Dampier foi um dos piratas que navegou pela costa brasileira, e a história sugere que ele teria enterrado baús contendo ouro, prata e joias em uma das praias da ilha. <br> <strong>Local:</strong> Algumas áreas específicas da ilha, como a Praia dos Portugueses, são consideradas pontos possíveis para o esconderijo de um tesouro. A região contém várias cavernas e enseadas escondidas, locais ideais para armazenar algo valioso.",
+        link: "https://pt.wikipedia.org/wiki/Tesouro_da_Trindade#:~:text=O%20tesouro%20est%C3%A1%20escondido%20numa,em%205%20milh%C3%B5es%20de%20libras.",
+        local:"",
+        tags: "tesouro ilha trindade"
+    },
+    {
+        titulo:"Tesouro da Ilha Oak",
+        descricao : "A lenda começou em 1795, quando um jovem chamado Daniel McGinnis encontrou uma depressão circular na ilha. Ele e outros moradores da região começaram a cavar e encontraram camadas de troncos e plataformas a cada três metros. O local, conhecido como 'Poço do Dinheiro', logo se tornou o foco de teorias sobre um tesouro enterrado. <br> <strong>Local:</strong> Ilha Oak.",
+        link: "https://pt.wikipedia.org/wiki/Ilha_Oak",
+        localPossivel:"",
+        tags: "tesouro ilha oak island"
+    },
+    {
+        titulo:"Tesouro do Capitão Olivier Levasseur",
+        descricao:"Conhecido como 'La Buse' (o abutre), Levasseur teria deixado um código para localizar seu tesouro antes de ser enforcado. A lenda diz que ele lançou um colar com um enigma que, se resolvido, levaria ao tesouro. <br> <strong>Local:</strong> Acredita-se que o tesouro esteja escondido em alguma parte da Ilha de Mahé, nas Seychelles, e há caçadores de tesouros que continuam tentando decifrar o enigma deixado por ele.",
+        link: "https://it.wikipedia.org/wiki/Olivier_Levasseur",
+        local:"",
+        tags:"tesouro olivier levasseur capitao capitã pirata"
+    },
+    {
+        titulo:"Tesouro de William Kidd",
+        descricao:"O Capitão William Kidd foi um pirata e corsário escocês do século XVII. Ele teria enterrado um grande tesouro, incluindo ouro e joias, supostamente para se proteger caso fosse preso. <br> <strong>Local:</strong> Há teorias de que parte do tesouro esteja escondida na ilha de Gardiners, perto de Long Island, em Nova York. Diversas expedições de busca ocorreram, mas nenhuma obteve sucesso em encontrar o famoso tesouro.",
+        link:"https://pt.wikipedia.org/wiki/William_Kidd#:~:text=Os%20bens%20capturados%20seriam%20divididos,e%2015%25%20para%20a%20tripula%C3%A7%C3%A3o.",
+        local:"",
+        tags:"tesouro william kidd pirata"
+    },
+    {
+        titulo:"Tesouro de Henry Morgan",
+        descricao:"Henry Morgan, um dos piratas mais famosos do Caribe, saqueou diversas cidades espanholas e teria escondido grande parte de seus saques. Dizem que ele acumulou riquezas significativas durante suas campanhas. <br> <strong>Local:</strong> Uma das principais especulações é que parte de seu tesouro esteja na costa do Panamá, em locais isolados e de difícil acesso.",
+        link:"https://pt.wikipedia.org/wiki/Henry_Morgan",
+        local:" ",
+        tags:"henry morgan fortuna tesouro pirata lenda"
+    }
+];
